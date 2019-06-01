8beac7513c98e35655224cb2f160b3ab829cdaa8
DarkEngineer <maniodark@gmail.com>
2019-06-02 00:10:33 +0200

    ShipBody thrust and rotation mechanics

--- ShipBody/ShipBody.gd
+++ ShipBody/ShipBody.gd
@@ -4,30 +4,60 @@ class_name ShipBody
 onready var animation = $Selection/Animation
 onready var selection = $Selection
 
-var _thrust : float = 0.0
+#movement variables
+export var _thrust: float = 0.0 setget , get_thrust
+var _acceleration: float = 5.0
+
+#rotation variables
 var _angular_velocity = 0.0
 var _angular_acceleration = 20.0
-var _max_angular_velocity : float = 10.0
+
+#limit variables
+var _max_thrust: float = 20.0
+var _max_angular_velocity: float = 10.0
+
+var _target = null
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	rotate_ship(delta)
+	_target = get_global_mouse_position()
+	rotate_ship(_target, delta)
+	move_forward(_target, _acceleration, delta)
+	
 
-func thrust(c_thrust : float, c_angle : float) -> Vector2 :
+
+func thrust(c_thrust: float, c_angle: float) -> Vector2:
 	var thruster = Vector2()
 	thruster.x = cos(c_angle) * c_thrust
 	thruster.y = sin(c_angle) * c_thrust
+	
 	return thruster
 
-func move_forward(c_thrust, c_angle, delta):
-	var t_thrust = thrust(c_thrust, c_angle)
-	var current_pos = get_position()
-	var next_pos = current_pos + t_thrust * delta
+# function for forward movement
+func move_forward(target_pos,c_acceleration, delta):
+	accelerate(c_acceleration, delta)
+	var limiter = create_thrust_limiter(target_pos)
+	var new_position = get_position() + thrust(get_thrust(), get_rotation()) * delta * limiter
+	set_position(new_position)
+	
 
+func accelerate(c_acceleration, delta):
+	_thrust += c_acceleration * delta
+	_thrust = min(_thrust, _max_thrust)
 
-func rotate_ship(delta):
+#add limiter to thrust
+func create_thrust_limiter(t_pos) -> float:
+	var distance_v = t_pos - get_global_position()
+	var direction = distance_v.normalized()
+	var ship_direction = Vector2(1, 0).rotated(get_rotation())
+	var dot = direction.dot(ship_direction)
+	dot = max(0.0, dot)
+	
+	return dot
+
+func rotate_ship(t_pos, delta):
 	var direction = (get_global_mouse_position() - get_global_position()).normalized()
 	var ship_direction = Vector2(1, 0).rotated(get_rotation())
 	var dot = direction.dot(ship_direction)
@@ -46,6 +76,9 @@ func rotate_ship(delta):
 			_angular_velocity = min(_angular_velocity, deg2rad(_max_angular_velocity))
 		
 		rotate(_angular_velocity * delta)
+
+func get_thrust():
+	return _thrust
 
 func select():
 	selection.show()
