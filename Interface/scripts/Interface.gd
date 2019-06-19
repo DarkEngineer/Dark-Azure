5787d9245eceb5efbfdd9b6f8f0365ff28e25f52
DarkEngineer <maniodark@gmail.com>
2019-06-19 17:11:30 +0200

    refactoring code

---
+++ Interface/scripts/Interface.gd
@@ -0,0 +1,35 @@
+extends Control
+
+""" 
+
+#####  Color Palette by Paletton.com
+#####  Palette URL: http://paletton.com/#uid=53M0u0k++HEuXZKRO+V+Wtg+amV
+
+
+*** Primary color:
+
+   shade 0 = #003ACC = rgb(  0, 58,204) = rgba(  0, 58,204,1) = rgb0(0,0.227,0.8)
+   shade 1 = #2260FC = rgb( 34, 96,252) = rgba( 34, 96,252,1) = rgb0(0.133,0.376,0.988)
+   shade 2 = #084EFF = rgb(  8, 78,255) = rgba(  8, 78,255,1) = rgb0(0.031,0.306,1)
+   shade 3 = #002D9D = rgb(  0, 45,157) = rgba(  0, 45,157,1) = rgb0(0,0.176,0.616)
+   shade 4 = #00237B = rgb(  0, 35,123) = rgba(  0, 35,123,1) = rgb0(0,0.137,0.482)
+
+*** Secondary color (1):
+
+   shade 0 = #3400CF = rgb( 52,  0,207) = rgba( 52,  0,207,1) = rgb0(0.204,0,0.812)
+   shade 1 = #5A24FC = rgb( 90, 36,252) = rgba( 90, 36,252,1) = rgb0(0.353,0.141,0.988)
+   shade 2 = #4608FF = rgb( 70,  8,255) = rgba( 70,  8,255,1) = rgb0(0.275,0.031,1)
+   shade 3 = #2800A0 = rgb( 40,  0,160) = rgba( 40,  0,160,1) = rgb0(0.157,0,0.627)
+   shade 4 = #20007D = rgb( 32,  0,125) = rgba( 32,  0,125,1) = rgb0(0.125,0,0.49)
+
+*** Secondary color (2):
+
+   shade 0 = #00B9C2 = rgb(  0,185,194) = rgba(  0,185,194,1) = rgb0(0,0.725,0.761)
+   shade 1 = #0AF0FB = rgb( 10,240,251) = rgba( 10,240,251,1) = rgb0(0.039,0.941,0.984)
+   shade 2 = #00F3FF = rgb(  0,243,255) = rgba(  0,243,255,1) = rgb0(0,0.953,1)
+   shade 3 = #00878D = rgb(  0,135,141) = rgba(  0,135,141,1) = rgb0(0,0.529,0.553)
+   shade 4 = #00696E = rgb(  0,105,110) = rgba(  0,105,110,1) = rgb0(0,0.412,0.431)
+"""
+
+func _ready():
+	pass
