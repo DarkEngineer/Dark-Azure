7d4dd20b3e860c557630a95bdaa8937863425dbf
DarkEngineer <maniodark@gmail.com>
2019-06-30 22:44:54 +0200

    GET functions for structures, using structure files in game (added functions)

--- PlanetTile/Field/Building.gd
+++ PlanetTile/Field/Building.gd
@@ -3,3 +3,9 @@ class_name Building
 
 export (int) var _building_time
 export (Dictionary) var _building_cost
+
+func get_building_time() -> int:
+	return _building_time
+
+func get_building_cost() -> Dictionary:
+	return _building_cost
--- PlanetTile/Field/Structure.gd
+++ PlanetTile/Field/Structure.gd
@@ -5,3 +5,15 @@ enum Building_Type {RESEARCH, INDUSTRY, MILITARY}
 
 export (Building_Type) var _type
 export (String) var _description
+
+func get_type():
+	return _type
+
+func get_description() -> String:
+	return _description
+
+func get_building_types():
+	var _array = []
+	for n in Building_Type:
+		_array.append(n)
+	return _array
