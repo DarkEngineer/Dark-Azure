5d1b8e0804e423ca9117958ce7215dc7d4f84761
DarkEngineer <maniodark@gmail.com>
2019-07-09 00:18:56 +0200

    added market offers and some simulation tests

--- Market.gd
+++ Market.gd
@@ -4,6 +4,8 @@ class_name Market
 var _sell_orders = []
 var _buy_orders = []
 
+var _traders = []
+
 var _next_order_id = 0
 
 enum ORDER_TYPES {SELL, BUY}
@@ -19,6 +21,34 @@ class Order:
 		_amount = order_amount
 		_price = order_price
 		_id = order_id
+	
+	func get_name():
+		return _name
+	
+	func get_amount() -> int:
+		return _amount
+	
+	func get_price():
+		return _price
+	
+	func get_id():
+		return _id
+	
+	func take_amount(units: int) -> bool:
+		var t_units = abs(units)
+		if t_units <= get_amount():
+			_amount -= t_units
+			return true
+		return false
+	
+	func check_order():
+		if get_amount() <= 0:
+			free()
+	
+
+class Trader:
+	var _orders = []
+	var _average_price = 0.0
 
 func _ready():
 	pass
@@ -41,9 +71,38 @@ func create_buy_order(o_name, o_amount, o_price, o_id):
 	var order_obj = create_order(o_name, o_amount, o_price, o_id)
 	_buy_orders.append(order_obj)
 
-func simulate_sell_orders(count):
-	for i in range(count):
-		var c_name = "Coal"
-		var c_amount = randi() % 4000 + 100
-		var c_price = randf() * 4.5 + 1.0
-		
+func order_check(buy_order, sell_order_array):
+	var buy_amount = buy_order.get_amount()
+	var buy_price = buy_order.get_price()
+	for sell_order in sell_order_array:
+		var sell_amount = sell_order.get_amount()
+		var sell_price = sell_order.get_price()
+		if buy_price >= sell_price:
+			if buy_amount <= sell_amount:
+				#resolve order
+				resolve_order(buy_order, sell_order)
+
+func resolve_order(buy_order: Order, sell_order: Order):
+	sell_order.take_amount(buy_order.get_amount())
+	buy_order.free()
+
+func simulate_sell_orders(units):
+	for i in range(units):
+		create_sell_order("Coal", randi() % 300 + 1, randf() * 3.5 + 0.01, get_next_order_id())
+
+func simulate_buy_orders(units):
+	for i in range(units):
+		create_buy_order("Coal", randi() % 300 + 1, randf() * 3.5 + 0.01, get_next_order_id())
+
+func calculate_average_sell_price():
+	var avg_price = 0.0
+	for order in _sell_orders:
+		avg_price += order.get_price()
+	avg_price = avg_price / float(_sell_orders.size())
+	return avg_price
+
+func _on_Timer_timeout():
+	simulate_sell_orders(randi() % 10)
+	simulate_buy_orders(randi() % 20)
+	$MarketInterface.update_sell_orders(_sell_orders)
+	print(calculate_average_sell_price())
--- PlanetTile/Market/MarketInterface.gd
+++ PlanetTile/Market/MarketInterface.gd
@@ -1,6 +1,8 @@
 extends Panel
 
 onready var _product_list = $Margin/VBox/MarketTabs/Offers/MarginLeft/ProductList
+onready var _sell_list = $Margin/VBox/MarketTabs/Offers/MarginRight/RightVBox/TabContainer/Offers/OfferVBox/SellOrders
+onready var _buy_list = $Margin/VBox/MarketTabs/Offers/MarginRight/RightVBox/TabContainer/Offers/OfferVBox/BuyOrders
 
 var _minerals = ["Coal"]
 
@@ -13,3 +15,15 @@ func show_products():
 	for obj in _minerals:
 		var products = _product_list.create_item(root)
 		products.set_text(0, obj) 
+
+func update_sell_orders(sell_orders):
+	var root = _sell_list.get_root()
+	if root as TreeItem:
+		root.free()
+	var titles = _sell_list.create_item()
+	titles.set_text(0, "Quantity")
+	titles.set_text(1, "Price")
+	for order in sell_orders:
+		var order_cell = _sell_list.create_item(titles)
+		order_cell.set_text(0, str(order.get_amount()))
+		order_cell.set_text(1, str(order.get_price()))
