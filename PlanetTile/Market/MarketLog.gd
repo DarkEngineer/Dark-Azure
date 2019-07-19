5566b63b863eb5958bb2f1cded7a4d9c5a369116
DarkEngineer <maniodark@gmail.com>
2019-07-20 00:20:17 +0200

    added new version of definition of the commodities class

--- PlanetTile/Market/MarketLog.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-onready var _logs = $Margin/VBox/Logs
-
-func order_created(order: Order):
-	_logs.add_text("Trader ID %d created order for %s - quantity %d for %.2f per unit" % [order.get_trader_id(), order.get_name(), order.get_amount(),
-		order.get_price()])
-	_logs.newline()
--- PlanetTile/Market/TradersPanel.gd
+++
@@ -1,4 +0,0 @@
-extends Panel
-
-func update_traders(traders_array: Array):
-	print("Traders: %s" % [str(traders_array)])
