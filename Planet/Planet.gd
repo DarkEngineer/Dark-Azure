a6e496e7092aa4eb0ff95279a47c2d0fe471aaff
DarkEngineer <maniodark@gmail.com>
2019-07-29 22:33:48 +0200

    commodity class for planet production

--- Planet/Planet.gd
+++ Planet/Planet.gd
@@ -6,16 +6,27 @@ var _buildings = []
 
 
 class Commodity:
-	var _name: String
-	var _
+	var _name: String setget , get_name
+	
+	func _init(n):
+		_name = n
+	
+	func get_name():
+		return _name
 
 class Building:
 	var _name: String
 	var _production = []
 	
-	func _init(n, p):
+	func _init(n):
 		_name = n
-		_production = p
+	
+	func add_production(c: Commodity, n: int):
+		var product = {
+			"commodity": c,
+			"quantity": n
+		}
+		_production.append(product)
 	
 
 func _ready():
