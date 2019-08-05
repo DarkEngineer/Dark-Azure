b09334ffd5443f0061cba493d0d7724eed971878
DarkEngineer <maniodark@gmail.com>
2019-08-05 23:03:35 +0200

    change item structure

--- global.gd
+++ global.gd
@@ -3,6 +3,13 @@ extends Node
 var _next_item_id: int = 0
 var _next_character_id: int = 0
 
+var _items_array = []
+
 func _ready():
 	pass
 
+func get_item_by_id(id):
+	for item in _items_array:
+		if id == item.get_id():
+			return id
+	return printerr("No such item with ID - %d" % [id])
