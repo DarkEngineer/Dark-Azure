4197e1b90cac5d0ba050c8b3e3c6a3531a945a66
DarkEngineer <maniodark@gmail.com>
2019-08-05 22:00:29 +0200

    added item definition

---
+++ Planet/Economy/DA_Item.gd
@@ -0,0 +1,16 @@
+extends Object
+class_name DA_Item
+
+var _name: String = "default_item"
+var _id: int = 0
+var _owner_id: int = 0
+var _quantity: float = 0
+
+func _init(name, owner_id = 0, quantity = 1):
+	_name = name
+	_owner_id = owner_id
+
+func get_next_item_id():
+	var id = global._next_item_id
+	global._next_item_id += 1
+	return id
--- global.gd
+++ global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var _next_item_id: int = 0
+
 func _ready():
 	pass
 
