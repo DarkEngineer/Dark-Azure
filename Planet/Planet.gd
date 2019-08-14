92068095c0c02ccc4aa64cc6e2448079841bb785
DarkEngineer <maniodark@gmail.com>
2019-08-14 19:28:18 +0200

    clear wrong basic structure

--- Planet/Planet.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-class_name Planet
-
-var _name: String = ""
-var _buildings = []
-
-
-func _ready():
-	pass
--- StructureBlueprints/R_StructureBlueprint.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-class_name R_StructureBlueprint
-
-export (String) var _structure_name_id: String = "default_structure"
-export (int) var _build_time: int
-export (Dictionary) var _dependency: Dictionary
-export (Dictionary) var _products: Dictionary
-
