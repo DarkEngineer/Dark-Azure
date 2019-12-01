5543ae4b18acddb611a22c63e13e57e9408c785d
DarkEngineer <maniodark@gmail.com>
2019-12-01 22:48:09 +0100

    added selection for units

--- src/Objects/StaticObject/StaticObject.gd
+++ src/Objects/StaticObject/StaticObject.gd
@@ -1,3 +1,11 @@
 extends StaticBody2D
 
 var _contain_resources: bool = false
+
+var _raw_minerals = []
+
+func add_raw_mineral(new_mineral):
+	_raw_minerals.append(new_mineral)
+
+func decrease_raw_mineral(mineral):
+	pass
